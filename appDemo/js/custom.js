10a38bfd5980071668c0e2b24a1c2edf17bce4b8
emilykrns94 <emilykrns@gmail.com>
2015-04-16 14:20:01 +0000

    fixed ajax url

--- appDemo/js/custom.js
+++ appDemo/js/custom.js
@@ -3,7 +3,13 @@ $(document).ready(function(){
       console.log("Your Name Button Has Been Clicked");
       $.ajax({
           method: "GET",
-      })
+          url:"//lit-cliffs-5322.herokuapp.com/api/yourName"
+      }).done(function(responce){
+          console.log(responce.name);   
+      }).fail(function(msg){
+          console.log("Error");
+          
+      });
                   
   });
 });
