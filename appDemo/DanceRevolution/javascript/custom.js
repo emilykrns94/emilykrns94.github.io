bd52410edefb79b958cb0dc4a7d148fd3d4bd586
emilykrns94 <emilykrns@gmail.com>
2015-04-21 11:52:04 +0000

    Dance Revolution Website

---
+++ appDemo/DanceRevolution/javascript/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function() 
+{
+	
+     $('.flexslider').flexslider(
+	 {
+		animation: "slide",
+		animationLoop: false,
+		pausePlay: true,
+        start: function(slider)
+		{
+          $('body').removeClass('loading');
+        }
+	 });
+});
--- appDemo/js/custom.js
+++ appDemo/js/custom.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
+   
+    $.ajax(
+          {
+          method: "GET",
+          url:"//lit-cliffs-5322.herokuapp.com/api/myName"
+          }).done(function(responce)
+            {
+          var theNamesHTML ="<ul id=\allNames\">";
+          for(var i = 0; i < responce.allNames.length; i++)
+              {
+                  theNamesHtml += "<li>" + responce.allNames[i] +"</li>";
+              }
+          console.log(responce.allNames); 
+          $('div#myNameResponce').html(theNamesHTML);
+            }).fail(function(msg){
+          console.log("Error");
+        });
+    
+ 
   $('button#yourNameButton').click(function(){
       console.log("Your Name Button Has Been Clicked");
       $.ajax({
@@ -6,11 +25,34 @@ $(document).ready(function(){
           url:"//lit-cliffs-5322.herokuapp.com/api/yourName"
       }).done(function(responce){
           console.log(responce.name); 
-          $('div#yourName').html("<p>"+responce.name"</p>");
+          $('div#yourName').html(responce.name);
       }).fail(function(msg){
           console.log("Error");
           
       });
                   
   });
+  $('button#myNameButton').click(function()
+   {
+      console.log("My Name Button Has Been Clicked");
+      $('input#myName').val("");
+      $.ajax(
+          {
+          method: "POST",
+          data:{myName: theName},
+          url:"//lit-cliffs-5322.herokuapp.com/api/myName"
+          }).done(function(responce)
+            {
+          var theNamesHTML ="<ul id=\allNames\">";
+          for(var i = 0; i < responce.allNames.length; i++)
+              {
+                  theNamesHtml += "<li>" + responce.allNames[i] +"</li>";
+              }
+          console.log(responce.allNames); 
+          $('div#myNameResponce').html(theNamesHTML);
+            }).fail(function(msg){
+          console.log("Error");
+           });
+                  
+    });
 });
